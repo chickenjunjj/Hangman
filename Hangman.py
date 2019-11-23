517c982dc38708eabc1a9305fe890fec517f99ff
Jun Johnson <jojon@Juns-MacBook-Air.local>
2019-11-23 12:40:05 +0900

    Second commit: The corrrect underscore doesn't replace

--- Hangman.py
+++ Hangman.py
@@ -1,17 +1,21 @@
 import random
-number = random.randrange(0, 5)
+number = random.randrange(0, 10)
 
-words = ["Yoshito", "Kai", "Ryo", "Kazuki", "Daniel"]
+words = ("table", "lunch", "seat", "bed", "drinks", "computer", "reading", "cake", "blue", "brown")
+
+print (words)
 
 word = (words[number])
 
-print (len(word) * "_")
+blanks = (len(word) * "_")
 
-print (word)
+print (len(word) * "_")
 
 print ("Guess a letter or the whole word")
 
 tries = 0
+
+correct = 0
 
 while tries < 8:
     guess = input()
@@ -20,9 +24,23 @@ while tries < 8:
         break
     elif -1 != word.find(guess):
         if len(guess) == 1:
-            print ("insane")
-            tries = tries + 1
             if tries < 8:
-                print ("Guess another letter or the word")
+                correct = correct + 1
+                if correct == len(word):
+                    print("Well Done!")
+                    break
+                if correct < len(word):
+                    find_result = word.find(guess)
+                    blanks = blanks.replace(blanks[(find_result)], guess, 1)
+                    print (blanks)
+                    print ("insane")
+                    print ("Guess another letter or the word")
         else:
             print ("Write only one letter or guess the whole letter")
+    elif -1 == word.find(guess):
+        tries = tries + 1
+        if tries == 8:
+                print ("Better luck next time")
+        print("You have " + str(8 - tries) + " tries left")
+    elif len(guess >1 and guess <len(word)):
+        print("guess only one letter or the whole word")
