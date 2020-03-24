cc2ed3d0c86f3d434ec5ef1f9cb6bc35166c42f4
Jun Johnson <jojon@Juns-MacBook-Air.local>
2020-03-24 15:22:26 +0900

    Used functions and fixed guessing system

--- Hangman.py
+++ Hangman.py
@@ -1,46 +1,85 @@
 import random
-number = random.randrange(0, 10)
+class Hangman():
+    def __init__(self):
+        self.start()
 
-words = ("table", "lunch", "seat", "bed", "drinks", "computer", "reading", "cake", "blue", "brown")
-
-print (words)
-
-word = (words[number])
-
-blanks = (len(word) * "_")
-
-print (len(word) * "_")
-
-print ("Guess a letter or the whole word")
-
-tries = 0
-
-correct = 0
-
-while tries < 8:
-    guess = input()
-    if guess == word:
-        print ("WOW")
-        break
-    elif -1 != word.find(guess):
-        if len(guess) == 1:
-            if tries < 8:
-                correct = correct + 1
-                if correct == len(word):
-                    print("Well Done!")
-                    break
-                if correct < len(word):
-                    find_result = word.find(guess)
-                    blanks = blanks.replace(blanks[(find_result)], guess, 1)
-                    print (blanks)
-                    print ("insane")
-                    print ("Guess another letter or the word")
+    def start(self):
+        print("[p] play")
+        print("[q] quit")
+        command = input()
+        print("")
+        if command == 'p':
+            self.play()
+        elif command == 'q':
+            pass
         else:
-            print ("Write only one letter or guess the whole letter")
-    elif -1 == word.find(guess):
-        tries = tries + 1
-        if tries == 8:
-                print ("Better luck next time")
-        print("You have " + str(8 - tries) + " tries left")
-    elif len(guess >1 and guess <len(word)):
-        print("guess only one letter or the whole word")
+            print("Invalid command \n")
+            self.start()
+
+    def play(self):
+        guess_list = []
+        number = random.randrange(0, 10) 
+        words = ["table", "lunch", "seat", "bed", "drinks", "computer", "reading", "cake", "blue", "brown"]
+        word = (words[number])
+        blanks = (len(word) * "_")
+        print (blanks)
+        tries = 0
+        correct = 0
+        while tries < 8:
+            print ("Guess a letter or the whole word \n")
+            guess = input()
+            indicies = self.find_indicies(guess, word)
+
+            
+            if len(guess) > 1:
+                if guess == word:
+                    print("You win! \n")
+                    break
+                else:
+                    print("You lose \n")
+                    break
+            elif len(guess) == 1:
+                if guess in guess_list:
+                    print("Already guessed letter")
+                elif len(indicies) > 0:
+                    guess_list.append(guess)
+                    correct = correct + 1
+                    blanks = self.replace_str(blanks, indicies, guess)
+                    print("")
+                    print (blanks + "\n")
+                    if correct == len(word):
+                        print("You win! \n")
+                        break
+                    elif correct < len(word):
+                        print("")
+                        print ("Insane")
+                else:
+                    guess_list.append(guess)
+                    print("Wrong answer")
+                    tries = tries + 1
+                    print("You have " + str(8 - tries) + " tries left \n")
+                    if tries == 8:
+                        print("You lose \n")
+                        break
+        self.start()
+                
+    def find_indicies(self, guess, word):
+        num = 0
+        indicies_list = []
+        for letter in word:
+            if guess == letter:
+                indicies_list.append(num)
+            else:
+                pass
+            num = num + 1
+        return indicies_list
+            
+
+    def replace_str(self, text, indicies, replacement):
+        for index in indicies:
+            text = "%s%s%s"%(text[:index], replacement, text[index + 1:])
+        return text
+    
+    
+
+hangman = Hangman()
